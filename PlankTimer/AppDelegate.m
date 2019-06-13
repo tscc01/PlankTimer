ec5e5096a17469d9b9895360e46a3eeea432e1e2
sola <tscc01@163.com>
2019-06-13 15:27:18 +0800

    change ui

--- PlankTimer/AppDelegate.m
+++ PlankTimer/AppDelegate.m
@@ -37,15 +37,15 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    id vcRoot = self.window.rootViewController;
-    if ([vcRoot isKindOfClass:[UINavigationController class]]) {
-        vcRoot = [[vcRoot viewControllers]lastObject];
-    }
-    
-    if ([vcRoot isKindOfClass:[MainContentViewController class]]) {
-        MainContentViewController *vcMain = vcRoot;
-        [vcMain initViews];
-    }
+//    id vcRoot = self.window.rootViewController;
+//    if ([vcRoot isKindOfClass:[UINavigationController class]]) {
+//        vcRoot = [[vcRoot viewControllers]lastObject];
+//    }
+//
+//    if ([vcRoot isKindOfClass:[MainContentViewController class]]) {
+//        MainContentViewController *vcMain = vcRoot;
+//        [vcMain initViews];
+//    }
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
